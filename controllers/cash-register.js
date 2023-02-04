0fea7a702119ec5a56718ef24722d5cb8c85a51a
Nicolas <hnpf2022@gmail.com>
2023-02-04 00:53:41 -0300

    lesson10

--- controllers/cash-register.js
+++ controllers/cash-register.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -9,32 +10,101 @@ const allCashRegister = async (req, res, next) => {
     });
 };
 
-const updateBuy = async (req, res) => {
-    try {
-      if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Invalid purchasing id.');
-      }
-      const buyId = new ObjectId(req.params.id);
-      // be aware of updateOne if you only want to update specific fields
-      const buy = {
-        revenue: req.body.revenue,
-        costLastPurchase: req.body.costLastPurchase
-      };
-      const response = await mongodb
+const findCash = async (id, reveneu, cost) => {
+  const reveneuu = reveneu + cost;
+  const costLastPurchasee = cost;
+  const buy = {
+    revenue: reveneuu,
+    costLastPurchase: costLastPurchasee
+  };
+  const response = await mongodb
+    .getDb()
+    .db('crazylibrary')
+    .collection('buy')
+    .replaceOne({ _id: id }, buy);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while getting the purchasing.');
+  }
+}
+
+
+  const updateBuy = async (req, res) => {
+    if(ObjectId.isValid(req.params.id)){
+      const userId = new ObjectId(req.params.id);
+      const result1 = await mongodb
         .getDb()
         .db('crazylibrary')
-        .collection('buy')
-        .replaceOne({ _id: buyId }, buy);
-      console.log(response);
-      if (response.modifiedCount > 0) {
-        res.status(204).send();
-      } else {
-        res.status(500).json(response.error || 'Some error occurred while getting the purchasing.');
-      }
-    } catch (err) {
-      res.status(500).json(err);
+        .collection('books')
+        .find({ _id: userId });
+      const result2 = await mongodb
+      .getDb()
+      .db('crazylibrary')
+      .collection('magazines')
+      .find({ _id: userId });
+      const result3 = await mongodb
+      .getDb()
+      .db('crazylibrary')
+      .collection('dictionaries')
+      .find({ _id: userId });
+      const cashInfo = await mongodb.getDb().db('crazylibrary').collection('cashregister').find();
+      const reveneu = 0;
+      const costLastPurchase = 0
+      cashInfo.toArray().then((lists) =>{
+        reveneu = json(lists[0].reveneu);
+        costLastPurchase = json(lists[0].costLastPurchase);
+
+     });
+      const cost = 0;
+        
+          result1.toArray().then((lists) => {
+
+            if (lists.length > 0){
+              cost = json(lists[0].price);
+              findCash(userId, reveneu, cost);
+
+            
+            }else{ 
+                  result2.toArray().then((lists) => {
+                   
+                    if (lists.length > 0){
+                      cost = json(lists[0].price);
+                      findCash(userId, reveneu, cost);
+
+                    }else{
+                        result3.toArray().then((lists) => {
+                          
+                          if (lists.length > 0){
+                            cost = json(lists[0].price);
+                            findCash(userId, reveneu, cost);
+
+                            
+                          }else{
+                            res.status(500).json('Some error occurred while finding the ID stated.');
+                          }
+                          
+                          
+                        })
+                    }
+                    
+                    
+                  })
+            }
+            
+            
+          })
+          
+       
+     } else {
+      res.status(500).json('Some error occurred while finding the ID stated.');
     }
-  };
+      
+    
+    };
+
+
 
 module.exports = { 
     allCashRegister, 
